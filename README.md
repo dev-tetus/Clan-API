78f6de2470cd344b7878cdc7acf7617f51e2e19c
Teo Roca <teo.roca798@gmail.com>
2022-02-28 23:07:42 +0100

    edge confi

---
+++ README.md
@@ -0,0 +1,1 @@
+venv -m discord-env
--- main.py
+++ main.py
@@ -1,42 +1,52 @@
-import discord
 import asyncio
 import logging
+from bs4 import BeautifulSoup
 
-logging.basicConfig(level=logging.INFO)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support.relative_locator import locate_with
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(
-    filename='./discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+
+
+url = "https://www.google.com"
+
+driver_path = r"./msedgedriver.exe"
+browser_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
+
+#Set-up chrome driver
+
+# options = webdriver.ChromeOptions()
+
+
+# options.binary_location = browser_path
+# options.add_argument(r'--user-data-dir=D:\Dev\Proyectos\discord-scraping\discord-env\Profile 2')
+# options.add_argument("--profile-directory=Profile 2")
 
 
 mail = 'tefilorofi@hotmail.com'
 password = 'Teoroca1998$'
 
 
-class DiscordClient(discord.Client):
-    def __init__(self, *args, **kwargs):
-        discord.Client.__init__(self, **kwargs)
-
-    async def on_ready(self):
-        servers = list(self.servers)
-        print('Logged on as {0}!'.format(self.user))
-        print('Printing servers for {0}...'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
-
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    print("Creando cliente discord...")
-    dc = DiscordClient()
-    dc.close()
-    print('Cliente creado...')
+    ser = Service(executable_path=driver_path)
+    driver=webdriver.Edge(service=ser)
+    driver.get("https://www.discord.com/login")
+    password_box = driver.find_element(by=By.NAME, value='email').send_keys("tetusrocus")
+    password_box = driver.find_element(by=By.NAME, value='password')
+    webdriver.ActionChains(driver).context_click(password_box).perform()
+    password_box.send_keys("cacacaca")
+    # submit_btn=driver.find_element(By.XPATH("//*[@id=app-mount]/div[2]/div/div/div/div/form/div/div/div[1]/div[2]/button[2]"))
+    submit_btn=driver.find_elements(By.TAG_NAME, "button")
+    for element in submit_btn:
+        if element.text == "Iniciar sesión":
+            button = element
+    webdriver.ActionChains(driver).click_and_hold(button).perform()
+    webdriver.ActionChains(driver).release().perform()
 
-    loop.run_until_complete(
-        dc.login('SSayaxidM2sT29K4fZJPLcD4te2GqLgz'))
-    loop.run_until_complete(dc.close())
+    # driver.quit()
+
+
+
+
+    
