f6a39e81478706c5c67f3e48b1d122ab6763c790
Teo Roca <teo.roca798@gmail.com>
2022-11-02 15:27:16 +0100

    Change to env variables

--- classes/credentials.py
+++ classes/credentials.py
@@ -1,26 +1,27 @@
 import sys
+import os
 
 def get_credentials():
 
-    if sys.platform == "Linux":
-        f = open('credentials-linux.txt', 'r')
-    else:
-        f = open('credentials.txt', 'r')
-    content = f.readlines()
+    # if sys.platform == "Linux":
+    #     f = open('credentials-linux.txt', 'r')
+    # else:
+    #     f = open('credentials.txt', 'r')
+    # content = f.readlines()
     credentials = {
-        'username': content[0].split(' ')[1].strip(),
-        'password': content[1].split(' ')[1].strip(),
-        'api_key': content[2].split(' ')[1].strip(),
-        'base_url': content[3].split(' ')[1].strip(),
-        'clan_tag': content[4].split(' ')[1].strip(),
-        'spells_csv': content[5].split(' ')[1].strip(),
-        'troops_csv': content[6].split(' ')[1].strip(),
-        'heroes_csv': content[7].split(' ')[1].strip(),
-        'buildings_csv': content[8].split(' ')[1].strip(),
-        'texts_csv': content[9].split(' ')[1].strip(),
-        'pets_csv': content[10].split(' ')[1].strip(),
-        'db_ip' : content[11].split(' ')[1].strip(),
-        'db_user' : content[12].split(' ')[1].strip(),
-        'db_pass' : content[13].split(' ')[1].strip(),
+        'username': os.environ['username'],
+        'password': os.environ['password'],
+        'api_key': os.environ['api_key'],
+        'base_url': os.environ['base_url'],
+        'clan_tag': os.environ['clan_tag'],
+        'spells_csv': os.environ['spells_csv'],
+        'troops_csv': os.environ['troops_csv'],
+        'heroes_csv': os.environ['heroes_csv'],
+        'buildings_csv': os.environ['buildings_csv'],
+        'texts_csv': os.environ['texts_csv'],
+        'pets_csv': os.environ['pets_csv'],
+        'db_ip' : os.environ['db_ip'],
+        'db_user' : os.environ['db_user'],
+        'db_pass' : os.environ['db_pass'],
     }
     return credentials
